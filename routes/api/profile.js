598378a02059d8bc1a5756c8f0dd7fd432734d62
Nadiia Katkova <nadezhda.katkova@gmail.com>
2019-02-14 14:55:59 +0200

    add more profile routes api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Input Validator
+const validateProfileInput = require("../../validation/profile");
+
+// load profile model
+const Profile = require("../../models/Profile");
+// load user profile
+const User = require("../../models/User");
 
 // @route   GET api/profile/test
 // @desc    Tests profile route
@@ -8,6 +18,161 @@ router.get("/test", (req, res) =>
   res.json({
     message: "Profile works!"
   })
+);
+
+// @route   GET api/profile
+// @desc    Current user profile route
+// @access  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
+      .then(profile => {
+        // there is no profile
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+
+        res.json(profile);
+      })
+      .catch(error => res.status(404).json(error));
+  }
+);
+
+// @route   GET api/profile/all
+// @desc    Get all users' profiles
+// @access  Public
+router.get("/all", (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      // there are no profiles
+      if (!profiles || profiles.length === 0) {
+        errors.noprofiles = "There are no users' profiles";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(error =>
+      res.status(404).json({ noprofiles: "There are no users' profiles" })
+    );
+});
+
+// @route   GET api/profile/handle/:handle
+// @desc    Get user's profile by handle
+// @access  Public
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      // there is no profile
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(error => res.status(404).json(error));
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get user's profile by ID
+// @access  Public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      // there is no profile
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(error =>
+      res.status(404).json({ noprofile: "There is no profile for this user" })
+    );
+});
+
+// @route   POST api/profile
+// @desc    Create or edit user profile
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    if (!isValid) {
+      // return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    // get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFieldy.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    // skills - plit in array
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+
+    // social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // update profile
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true, useFindAndModify: false }
+        )
+          .populate("user", ["name", "avatar"])
+          .then(profile => res.json(profile));
+      } else {
+        // create profile
+
+        // check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "This handle already exists";
+            res.status(400).json(errors);
+          }
+
+          // save new profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
+  }
 );
 
 module.exports = router;
---
+++ validation/profile.js
@@ -0,0 +1,79 @@
+const validator = require("validator");
+const isEmpty = require("./isEmty");
+
+module.exports = function validateProfileInput(data) {
+  let {
+    handle,
+    status,
+    skills,
+    website,
+    youtube,
+    twitter,
+    linkedin,
+    instagram,
+    facebook
+  } = data;
+
+  let errors = {};
+
+  handle = !isEmpty(handle) ? handle : "";
+  status = !isEmpty(status) ? status : "";
+  skills = !isEmpty(skills) ? skills : "";
+
+  if (!validator.isLength(handle, { min: 2, max: 30 })) {
+    errors.handle = "Handle must be between 2 and 4 characters";
+  }
+
+  if (validator.isEmpty(handle)) {
+    errors.handle = "Profile handle is required";
+  }
+
+  if (validator.isEmpty(status)) {
+    errors.status = "Status field is required";
+  }
+
+  if (validator.isEmpty(skills)) {
+    errors.skills = "Skills field is required";
+  }
+
+  if (!isEmpty(website)) {
+    if (!validator.isURL(website)) {
+      errors.website = "Not a valid URL";
+    }
+  }
+
+  if (!isEmpty(youtube)) {
+    if (!validator.isURL(youtube)) {
+      errors.youtube = "Not a valid youtube URL";
+    }
+  }
+
+  if (!isEmpty(twitter)) {
+    if (!validator.isURL(twitter)) {
+      errors.twitter = "Not a valid twitter URL";
+    }
+  }
+
+  if (!isEmpty(linkedin)) {
+    if (!validator.isURL(linkedin)) {
+      errors.linkedin = "Not a valid linkedin URL";
+    }
+  }
+
+  if (!isEmpty(instagram)) {
+    if (!validator.isURL(instagram)) {
+      errors.instagram = "Not a valid instagram URL";
+    }
+  }
+
+  if (!isEmpty(facebook)) {
+    if (!validator.isURL(facebook)) {
+      errors.facebook = "Not a valid facebook URL";
+    }
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
