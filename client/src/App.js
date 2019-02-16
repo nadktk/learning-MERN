6510fa533e36b887abc53198df9f5f16236127a1
Nadiia Katkova <nadezhda.katkova@gmail.com>
2019-02-16 02:12:58 +0200

    innitial client side

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use("/api/user", user);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
