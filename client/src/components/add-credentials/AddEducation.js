cb5472e08200b159ae4341ec9e579a828f078052
Nadiia Katkova <nadezhda.katkova@gmail.com>
2019-03-01 12:01:58 +0200

    add add education form page

--- client/src/components/add-credentials/AddEducation.js
+++ client/src/components/add-credentials/AddEducation.js
@@ -1,41 +1,156 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
-import { TextFieldGroup } from "../common/TextFieldGroup";
-import { TextAreaFieldGroup } from "../common/TextAreaFieldGroup";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { addEducation } from "../../actions/profileActions";
+
+import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 
 class AddEducation extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      company: "",
-      title: "",
-      location: "",
+      school: "",
+      degree: "",
+      fieldofstudy: "",
       from: "",
       to: "",
       description: "",
       errors: {},
-      disabled: false
+      current: false
     };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onCheck = this.onCheck.bind(this);
   }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.errors && nextProps.errors !== prevState.errors) {
+      return { errors: nextProps.errors };
+    } else return null;
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const eduData = {
+      school: this.state.school,
+      degree: this.state.degree,
+      fieldofstudy: this.state.fieldofstudy,
+      from: this.state.from,
+      to: this.state.to,
+      current: this.state.current,
+      description: this.state.description
+    };
+
+    this.props.addEducation(eduData, this.props.history);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onCheck(e) {
+    this.setState({
+      current: !this.state.current
+    });
+  }
+
   render() {
-    const { errors } = this.state;
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+      errors
+    } = this.state;
     return (
-      <div className="add-experience">
+      <div className="add-education full-height">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
               <Link to="/dashboard" className="btn btn-light">
                 Go Back
               </Link>
-              <h1 className="display-4 text-center">Add Experience</h1>
+              <h1 className="display-4 text-center">Add Education</h1>
               <p className="lead text-center">
-                Add any job or position that you have had in the past or current
+                Add any school, bootcamp, etc that you have attended
               </p>
               <small className="d-block pb-3 text-danger">
                 * = required fields
               </small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* School"
+                  name="school"
+                  value={school}
+                  onChange={this.onChange}
+                  error={errors.school}
+                />
+                <TextFieldGroup
+                  placeholder="* Degree or Certificate"
+                  name="degree"
+                  value={degree}
+                  onChange={this.onChange}
+                  error={errors.degree}
+                />
+                <TextFieldGroup
+                  placeholder="* Field of Study"
+                  name="fieldofstudy"
+                  value={fieldofstudy}
+                  onChange={this.onChange}
+                  error={errors.fieldofstudy}
+                />
+                <h6>* From Date</h6>
+                <TextFieldGroup
+                  type="date"
+                  name="from"
+                  value={from}
+                  onChange={this.onChange}
+                  error={errors.from}
+                />
+                <h6>To Date</h6>
+                <TextFieldGroup
+                  type="date"
+                  name="to"
+                  value={to}
+                  onChange={this.onChange}
+                  error={errors.to}
+                  disabled={current ? "disabled" : ""}
+                />
+                <div className="form-check mb-4">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    name="current"
+                    id="current"
+                    value={current}
+                    checked={current}
+                    onChange={this.onCheck}
+                  />
+                  <label htmlFor="current" className="form-check-label">
+                    Current Program
+                  </label>
+                </div>
+                <TextAreaFieldGroup
+                  placeholder="Program description"
+                  name="description"
+                  value={description}
+                  onChange={this.onChange}
+                  error={errors.description}
+                  info="Tell us about the position"
+                />
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-info btn-block mt-4"
+                />
+              </form>
             </div>
           </div>
         </div>
@@ -46,7 +161,8 @@ class AddEducation extends Component {
 
 AddEducation.propTypes = {
   profile: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
+  errors: PropTypes.object.isRequired,
+  addEducation: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -54,4 +170,7 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(withRouter(AddEducation));
+export default connect(
+  mapStateToProps,
+  { addEducation }
+)(withRouter(AddEducation));
