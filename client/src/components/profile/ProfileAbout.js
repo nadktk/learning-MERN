fef3e2875a59c74eb03ee3223576c9cb3f9087b5
Nadiia Katkova <nadezhda.katkova@gmail.com>
2019-03-05 13:50:44 +0200

    add profile page elements

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,14 +1,50 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+
+    // find the first name
+    const firstName = profile.user.name.split(" ")[0];
+
+    // skills list
+    const skills = profile.skills.map((skill, ind) => (
+      <div key={ind} className="p-3">
+        <i className="fa fa-check" />
+        {" " + skill}
+      </div>
+    ));
+
     return (
       <div>
-        <h1>TODO: PROFILE ABOUT</h1>
-        {/* put information about profile here */}
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card card-body bg-light mb-3">
+              <h3 className="text-center text-info">{firstName}'s Bio</h3>
+              {isEmpty(profile.bio) ? (
+                <p className="lead">{firstName + " doesn't have a bio"}</p>
+              ) : (
+                <p className="lead">{profile.bio}</p>
+              )}
+              <hr />
+              <h3 className="text-center text-info">Skill Set</h3>
+              <div className="row">
+                <div className="d-flex flex-wrap justify-content-center align-items-center">
+                  {skills}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
 
 export default ProfileAbout;
--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,14 +1,97 @@
 import React, { Component } from "react";
+import Moment from "react-moment";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileCreds extends Component {
   render() {
+    const { experience, education } = this.props;
+
+    const expItems = experience.map(exp => (
+      <li key={exp._id} className="list-group-item">
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="DD/MM/YYYY">{exp.from}</Moment> {" - "}
+          {exp.current ? "Now" : <Moment format="DD/MM/YYYY">{exp.to}</Moment>}
+        </p>
+        <p>
+          <strong>Position: </strong>
+          {exp.title}
+        </p>
+        {isEmpty(exp.location) ? null : (
+          <p>
+            <strong>Location: </strong>
+            {exp.location}
+          </p>
+        )}
+        {isEmpty(exp.description) ? null : (
+          <p>
+            <strong>Description: </strong>
+            {exp.description}
+          </p>
+        )}
+      </li>
+    ));
+
+    const eduItems = education.map(edu => (
+      <li key={edu._id} className="list-group-item">
+        <h4>{edu.school}</h4>
+        <p>
+          <Moment format="DD/MM/YYYY">{edu.from}</Moment> {" - "}
+          {edu.current ? "Now" : <Moment format="DD/MM/YYYY">{edu.to}</Moment>}
+        </p>
+        <p>
+          <strong>Position: </strong>
+          {edu.degree}
+        </p>
+        <p>
+          <strong>Field of Study: </strong>
+          {edu.fieldofstudy}
+        </p>
+        {isEmpty(edu.description) ? null : (
+          <p>
+            <strong>Description: </strong>
+            {edu.description}
+          </p>
+        )}
+      </li>
+    ));
+
     return (
-      <div>
-        <h1>TODO: PROFILE CREDENTIEALS</h1>
-        {/* put information about profile credentials here */}
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          <ul className="list-group">
+            {expItems[0] ? (
+              expItems
+            ) : (
+              <li className="list-group-item">
+                <h4>No experience</h4>
+              </li>
+            )}
+          </ul>
+        </div>
+
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          <ul className="list-group">
+            {eduItems[0] ? (
+              eduItems
+            ) : (
+              <li className="list-group-item">
+                <h4>No education</h4>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     );
   }
 }
+
+ProfileCreds.propTypes = {
+  experience: PropTypes.array,
+  education: PropTypes.array
+};
 
 export default ProfileCreds;
